95c3cb0b6bedda65a1d3b1ad6d657d12bdd4906c
duogwas <duong2001n@gmail.com>
2024-01-22 14:20:54 +0700

    done feat/intro

--- app/src/main/java/fithou/duogwas/goldie/Activity/IntroActivity.java
+++ app/src/main/java/fithou/duogwas/goldie/Activity/IntroActivity.java
@@ -2,15 +2,37 @@ package fithou.duogwas.goldie.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 import fithou.duogwas.goldie.R;
 
 public class IntroActivity extends AppCompatActivity {
+    TextView tvStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_intro);
+        AnhXa();
+        tvStartClick();
+    }
+
+    private void AnhXa() {
+        tvStart = findViewById(R.id.tvStart);
+    }
+
+    private void tvStartClick() {
+        tvStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this, SignInActivity.class));
+                finish();
+            }
+        });
     }
 }
